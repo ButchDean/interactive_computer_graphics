cdd8afad2426ecb98c63b59c3b161e6920ae1c56
Dean Butcher <butchdeanca@gmail.com>
2019-03-06 20:22:07 -0400

    2018 chapter 3 updates.

--- chap_03/LINUX_VERSIONS/example2.cpp
+++ chap_03/LINUX_VERSIONS/example2.cpp
@@ -193,7 +193,6 @@ main( int argc, char **argv )
     glutInitContextProfile( GLUT_CORE_PROFILE );
     glutCreateWindow( "Color Cube" );
 
-	glewExperimental = GL_TRUE;
     glewInit();
 
     init();
